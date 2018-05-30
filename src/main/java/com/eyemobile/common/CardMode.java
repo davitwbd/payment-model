9a8fae39823d903cb86c299f847233ba187a9edf
Davi <davi.pereira@eyemobile.com.br>
2018-05-30 11:28:53 -0300

    Atualização (CardMode)

--- src/main/java/com/eyemobile/common/CardMode.java
+++ src/main/java/com/eyemobile/common/CardMode.java
@@ -5,6 +5,22 @@ public enum CardMode {
     CREDIT(1);
 
     private final int id;
-    CardMode(int id) { this.id = id; }
-    public int getValue() { return id; }
+
+    CardMode(int id) {
+        this.id = id; }
+
+    public int getValue() {
+        return id;
+    }
+
+    public static CardMode getEnum(int id){
+        switch (id) {
+            case 0:
+                return DEBIT;
+            case 1:
+                return CREDIT;
+            default:
+                return CREDIT;
+        }
+    }
 }
